43e025409127c5dae747c49f95e21d3b6f75b0ab
TheSet40 <jonatan0402@oulook.com>
2023-06-24 19:12:41 +0200

    Add project files.

---
+++ Unblocker/Program.cs
@@ -0,0 +1,21 @@
+﻿
+while (true)
+{
+    Console.Write("write path to main folder: ");
+    string originalpath = Console.ReadLine()!;
+    try
+    {
+        string[] dirs = Directory.GetDirectories(originalpath, "*", SearchOption.AllDirectories);
+        Console.WriteLine("working...");
+        foreach (string dir in dirs)
+        {
+            Powershellhandler.Command($"get-childitem \"{dir}\" | unblock-File");
+        }
+        Powershellhandler.Command($"get-childitem \"{originalpath}\" | unblock-File");
+        Console.WriteLine("work completed\n");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"non valid directory!\nerror: {ex.ToString().Split('\n')[0]}\n");
+    }
+}
---
+++ Unblocker/powershell_handler.cs
@@ -0,0 +1,19 @@
+﻿using System.Management.Automation;
+
+public static class Powershellhandler
+{
+    private readonly static PowerShell PS = PowerShell.Create();
+
+    public static void Command(string script)
+    {
+        PS.AddScript(script);
+
+        PS.AddCommand("Out-string");
+
+        IAsyncResult result = PS.BeginInvoke<PSObject, PSObject>(null, null);
+
+        PS.EndInvoke(result);
+
+        PS.Commands.Clear();
+    }
+}
