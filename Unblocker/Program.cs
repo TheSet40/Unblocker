5182bc7b9adb8804d0edf6983faaff146276e650
TheSet40 <jonatan0402@oulook.com>
2023-08-08 03:46:28 +0200

    added loading animation

--- Unblocker/Program.cs
+++ Unblocker/Program.cs
@@ -1,27 +1,54 @@
-﻿using System.Collections.ObjectModel;
-using System.Management.Automation;
+﻿
 
-while (true)
+namespace Unblocker
 {
-    Console.WriteLine("use -v for output info");
-    Console.Write("write path to main folder: ");
-    string originalpath = Console.ReadLine()!.Trim();
-    bool shouldLog = originalpath.EndsWith("-v");
-    if (shouldLog) originalpath = originalpath.Split('-')[0].Trim();
-    try
+    class UnblockerProgram
     {
-        string[] dirs = Directory.GetDirectories(originalpath, "*", SearchOption.AllDirectories);
-        Console.WriteLine("working...");
-        foreach (string dir in dirs)
+        static async Task DisplayWorking(string message, CancellationToken cancellationToken)
         {
-            if (shouldLog) { Console.WriteLine(dir); }
-            Powershellhandler.Command($"get-childitem \"{dir}\" | unblock-File");
+            byte i = 0;
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                Console.Clear();
+                Console.WriteLine(message + new string('.', i % 4));
+                i++;
+                i %= 4;
+                await Task.Delay(250);
+            }
         }
-        Powershellhandler.Command($"get-childitem \"{originalpath}\" | unblock-File");
-        Console.WriteLine("work completed\n");
-    }
-    catch (Exception error)
-    {
-        Console.WriteLine($"non valid directory!\nerror: {error}\n");
+        static async Task Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("use -v for output info");
+                Console.Write("write path to main folder: ");
+                string originalpath = Console.ReadLine()!.Trim();
+                bool shouldLog = originalpath.EndsWith("-v");
+                if (shouldLog) originalpath = originalpath.Split('-')[0].Trim();
+                try
+                {
+                    var ctsDisplay = new CancellationTokenSource();
+                    var displayTask = DisplayWorking("checking directories", ctsDisplay.Token);
+                    string[] dirs = Directory.GetDirectories(originalpath!, "*", SearchOption.AllDirectories);
+                    ctsDisplay.Cancel();
+                    await displayTask;
+                    var ctsWorking = new CancellationTokenSource();
+                    if (!shouldLog) displayTask = DisplayWorking("working", ctsWorking.Token);
+                    for (long i = 0; i < dirs.Length; i++)
+                    {
+                        if (shouldLog) Console.WriteLine(dirs[i]);
+                        Powershellhandler.Command($"get-childitem \"{dirs[i]}\" | unblock-File");
+                    }
+                    Powershellhandler.Command($"get-childitem \"{originalpath}\" | unblock-File");
+                    ctsWorking.Cancel();
+                    await displayTask;
+                    Console.WriteLine("Work completed\n");
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine($"Non valid directory!\nError: {error}\n");
+                }
+            }
+        }
     }
 }
